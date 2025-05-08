ee035e58fff0e46c8c0d2758942cfa6393445969
John Emmanuel Sales <salesjohnemmanuel@gmail.com>
2025-05-08 19:57:25 +0800

    Created input for users

--- file-encryptor/src/main.rs
+++ file-encryptor/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::{self, Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("File Encryptor!");
+    println!("1. Encrypt a File");
+    println!("2. Decrypt a File");
+    print!("Choose an option: ");
+    io::stdout().flush().unwrap();
+
+    let mut choice = String::new();
+    io::stdin().read_line(&mut choice).unwrap();
+
+    if choice != "1" && choice != "2"{
+        println!("Not a valid choice!");
+        return;
+    }
+
 }
